1a005b086db22d1342a7fb6f7f5594c217f98f70
Twaddler01 <kenleydad@gmail.com>
2026-09-07 16:54:52 -0500

    Debug Phaser eSim ES MODULES

--- app/src/main/java/com/twaddler/esimtest/MainActivity.java
+++ app/src/main/java/com/twaddler/esimtest/MainActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.annotation.Nullable;
+import androidx.webkit.WebViewAssetLoader;
+
 public class MainActivity extends Activity {
+
     private WebView webView;
 
     @Override
@@ -24,22 +30,58 @@ public class MainActivity extends Activity {
         );
 
         webView = new WebView(this);
+
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setAllowFileAccess(true);
-        webView.getSettings().setAllowContentAccess(true);
+
+        webView.getSettings().setAllowFileAccess(false);
+        webView.getSettings().setAllowContentAccess(false);
+
         webView.getSettings().setBuiltInZoomControls(false);
         webView.getSettings().setDisplayZoomControls(false);
 
+        WebViewAssetLoader assetLoader =
+                new WebViewAssetLoader.Builder()
+                        .addPathHandler(
+                                "/assets/",
+                                new WebViewAssetLoader.AssetsPathHandler(this)
+                        )
+                        .build();
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(
+                    WebView view,
+                    WebResourceRequest request
+            ) {
+                return assetLoader.shouldInterceptRequest(request.getUrl());
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(
+                    WebView view,
+                    String url
+            ) {
+                return assetLoader.shouldInterceptRequest(
+                        android.net.Uri.parse(url)
+                );
+            }
+        });
+
         setContentView(webView);
-        webView.loadUrl("file:///android_asset/index.html");
+
+        webView.loadUrl(
+                "https://appassets.androidplatform.net/assets/index.html"
+        );
     }
 
     @Override
     public void onBackPressed() {
+
         if (webView != null && webView.canGoBack()) {
             webView.goBack();
         } else {
