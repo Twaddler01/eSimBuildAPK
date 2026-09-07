5d79550c3f9643dbcde1666929b87f1d3d289136
Twaddler01 <kenleydad@gmail.com>
2026-09-07 16:20:19 -0500

    Debug Phaser eSim ES MODULES

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -3,12 +3,24 @@ import TestScene from './TestScene.js';
 document.getElementById("main").innerHTML = `
     <div style="
         color: white;
+        background-color: #111111;
         font-family: Arial;
         font-size: 30px;
         text-align: center;
         margin-top: 100px;
     ">
-        main.js works!  ...3
+        main.js works!  ...
+    </div>
+    
+    <div style="
+        color: black;
+        background-color: white;
+        font-family: Arial;
+        font-size: 30px;
+        text-align: center;
+        margin-top: 100px;
+    ">
+        main.js works!  ...
     </div>
 `;
 
